8ea7cfa9c028bb5db018e26877dd0a4129e231f1
ewa1do <this.eduardovera@gmail.com>
2022-06-13 13:22:27 -0400

    tarea 2

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(value);
 
   // handleAdd
   const handleAdd = () => {
     setCounter(counter + 1);
     // setCounter(count => count + 1);
+  };
+
+  const handleSubstract = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
   };
 
   return (
@@ -16,6 +24,8 @@ const CounterApp = ({ value }) => {
       <h2> {counter} </h2>
 
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSubstract}>-1</button>
+      <button onClick={handleReset}>RESET</button>
     </>
   );
 };
