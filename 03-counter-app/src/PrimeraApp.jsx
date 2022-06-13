2f9dde78980e186a4c1e8bc984f0e87991097bbf
ewa1do <this.eduardovera@gmail.com>
2022-06-13 10:42:44 -0400

    impresion de variables en componentes

--- 03-counter-app/src/PrimeraApp.jsx
+++ 03-counter-app/src/PrimeraApp.jsx
@@ -1,7 +1,19 @@
+// import React, { Fragment } from 'react';
 import React from 'react';
 
 const PrimeraApp = () => {
-  return <h1>Hello World!</h1>;
+  const greeting = {
+    name: 'Eduardo',
+    age: 24,
+  };
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      {/* <pre>{JSON.stringify(greeting, null, 3)}</pre> */}
+      <h2>My first app</h2>
+    </>
+  );
 };
 
 export default PrimeraApp;
