c40a416125ff9792c16b502d10c2b3ca82323e7e
ewa1do <this.eduardovera@gmail.com>
2022-06-13 12:26:51 -0400

    tarea 1

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 import './main.css';
 
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <PrimeraApp greeting="Hello world!" />
+    {/* <PrimeraApp greeting="Hello world!" /> */}
+    <CounterApp value={23} />
   </React.StrictMode>
 );
