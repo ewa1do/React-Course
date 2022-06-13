8c2d84461a5a8f00df2e516cb467e02969ef46c5
ewa1do <this.eduardovera@gmail.com>
2022-06-12 21:25:38 -0400

    hola mundo en react

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const greeting = <h1>Hello World!</h1>;
+
+const root = document.getElementById('root');
+
+console.log(root);
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <h1>{greeting}</h1>
   </React.StrictMode>
-)
+);
