d57f75545358bf7d301397dccb48e3af979cbe82
ewa1do <this.eduardovera@gmail.com>
2022-06-13 17:41:15 -0400

    tests: jest expect and toBe

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,7 +1,12 @@
-test('Must be true', () => {
-  const isActive = true;
+describe('Tests on demo.test.js', () => {
+  test('The strings must be equals', () => {
+    // 1. Inicializacion
+    const message1 = 'Hello World';
 
-  if (isActive) {
-    throw new Error('Its not active');
-  }
+    // 2. Estimulo
+    const message2 = `Hello World`;
+
+    // 3. Observar el comportamiento
+    expect(message1).toBe(message2);
+  });
 });
