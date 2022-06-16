bff6cb0a07d3960e1fc8dfbe602e5797ed3bac3f
ewa1do <this.eduardovera@gmail.com>
2022-06-16 13:32:38 -0400

    useState

--- hooks-app/src/main.jsx
+++ hooks-app/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HookApp from './HookApp';
+import CounterApp from './components/01-useState/CounterApp';
+// import HookApp from './HookApp';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <HookApp />
+  // <HookApp />
+  <CounterApp />
 );
