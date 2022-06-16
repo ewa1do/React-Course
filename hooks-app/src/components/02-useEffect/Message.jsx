ec640e72a02a5b209171f2f75c87704ba4feb091
ewa1do <this.eduardovera@gmail.com>
2022-06-16 18:02:09 -0400

    useEffect precauciones

--- hooks-app/src/components/02-useEffect/Message.jsx
+++ hooks-app/src/components/02-useEffect/Message.jsx
@@ -1,17 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Message = () => {
-  useEffect(() => {
-    console.log('Component mount');
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
 
+  const { x, y } = coords;
+
+  useEffect(() => {
+    // * the first part of use effect is the side effect we wanna include
+
+    const mouseMove = (e) => {
+      const coords = { x: e.pageX, y: e.pageY };
+
+      setCoords(coords);
+    };
+
+    window.addEventListener('mousemove', mouseMove);
+
+    // * The return is the 'cleanup' we use it when we want to avoid several requests
     return () => {
-      console.log('Component unmount');
+      window.removeEventListener('mousemove', mouseMove);
     };
   }, []);
 
   return (
     <div>
       <h3>sos kul</h3>
+
+      <p>
+        X {x} Y: {y}
+      </p>
     </div>
   );
 };
