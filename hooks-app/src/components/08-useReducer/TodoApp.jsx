f93955e327de2a31f7f085d7020f5e94c16e61cc
ewa1do <this.eduardovera@gmail.com>
2022-06-18 20:13:56 -0400

    estilizando el componente TodoApp

--- hooks-app/src/components/08-useReducer/TodoApp.jsx
+++ hooks-app/src/components/08-useReducer/TodoApp.jsx
@@ -19,14 +19,52 @@ const TodoApp = () => {
 
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>
+        TodoApp <small>({todos.length})</small>
+      </h1>
       <hr />
 
-      <ul>
-        <li>Hola</li>
-        <li>Mundo</li>
-        <li>React</li>
-      </ul>
+      <div className='row'>
+        <div className='col-7'>
+          <ul className='list-group'>
+            {todos.map(({ id, desc }, i) => {
+              return (
+                <li
+                  key={id}
+                  className='list-group-item'
+                >
+                  <p className='text-center'>
+                    {i + 1}. {desc}
+                  </p>
+
+                  <button className='btn btn-danger'>
+                    Borrar
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className='col-5'>
+          <h4>Agregar TODO</h4>
+          <hr />
+
+          <form>
+            <input
+              type='text'
+              name='description'
+              placeholder='Aprender...'
+              autoComplete='off'
+              className='form-control'
+            />
+
+            <button className='btn btn-outline-primary mt-2 btn-block'>
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
