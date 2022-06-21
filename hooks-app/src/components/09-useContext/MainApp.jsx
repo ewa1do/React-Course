851bfabd6001533fb9b48df688646384ff49c565
ewa1do <this.eduardovera@gmail.com>
2022-06-21 13:58:23 -0400

    createContext y useContext

--- hooks-app/src/components/09-useContext/MainApp.jsx
+++ hooks-app/src/components/09-useContext/MainApp.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 import AppRouter from './AppRouter';
-
+import { UserContext } from './UserContext';
 const MainApp = () => {
-  return <AppRouter />;
+  const user = {
+    id: 1234,
+    name: 'Eduardo',
+    email: 'eduardo@vera.com',
+  };
+
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
 
 export default MainApp;
