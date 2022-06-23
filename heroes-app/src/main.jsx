4b192d326fa58fc9544987b63b40a5fa970c0e69
ewa1do <this.eduardovera@gmail.com>
2022-06-23 13:20:02 -0400

    inicio de proyecto heroes-app

---
+++ heroes-app/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import HeroesApp from './HeroesApp';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <HeroesApp />
+);
