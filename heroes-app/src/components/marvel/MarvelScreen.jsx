60f34278feebfbd0cd0830ef6b4e1a58257fc56e
ewa1do <this.eduardovera@gmail.com>
2022-06-23 13:52:05 -0400

    creando un primer router

---
+++ heroes-app/src/components/marvel/MarvelScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MarvelScreen = () => {
+  return (
+    <div>
+      <h1>MarvelScreen</h1>
+    </div>
+  );
+};
+
+export default MarvelScreen;
---
+++ heroes-app/src/routers/AppRouter.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import DcScreen from '../components/dc/DcScreen';
+import LoginScreen from '../components/login/LoginScreen';
+import MarvelScreen from '../components/marvel/MarvelScreen';
+import SearchScreen from '../components/search/SearchScreen';
+import { Navbar } from '../components/UI/Navbar';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path='/'
+          element={<MarvelScreen />}
+        />
+        <Route
+          path='/marvel'
+          element={<MarvelScreen />}
+        />
+        <Route
+          path='dc'
+          element={<DcScreen />}
+        />
+        <Route
+          path='search'
+          element={<SearchScreen />}
+        />
+        <Route
+          path='login'
+          element={<LoginScreen />}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
