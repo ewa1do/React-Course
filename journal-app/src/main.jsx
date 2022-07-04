180340976a642e561ff43aa524e749d461094156
ewa1do <this.eduardovera@gmail.com>
2022-07-04 17:46:11 -0400

    adding project  starting files

---
+++ journal-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { JournalApp } from './JournalApp';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <JournalApp />
+  </React.StrictMode>
+);
