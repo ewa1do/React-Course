ec81d9c8c2fc9c6edca9fe992295d59898c6e50c
ewa1do <this.eduardovera@gmail.com>
2022-07-05 13:44:04 -0400

    Estructura y diseno del HomeScreen

--- journal-app/src/components/notes/NoteScreen.jsx
+++ journal-app/src/components/notes/NoteScreen.jsx
@@ -4,6 +4,26 @@ export const NoteScreen = () => {
   return (
     <div className='notes__main-content'>
       <NotesAppBar />
+
+      <div className='notes__content'>
+        <input
+          type='text'
+          placeholder='Some awesome title'
+          className='notes__title-input'
+          autoComplete='off'
+        />
+        <textarea
+          placeholder='What happened today?'
+          className='notes__textarea'
+        ></textarea>
+
+        <div className='notes__image'>
+          <img
+            src='https://www.whatsappimages.in/wp-content/uploads/2021/07/Top-HD-sad-quotes-for-whatsapp-status-in-hindi-Pics-Images-Download-Free.gif'
+            alt='image'
+          />
+        </div>
+      </div>
     </div>
   );
 };
