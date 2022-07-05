6bfbdf38659d20da7c61fda688438a0e3f03bdb2
ewa1do <this.eduardovera@gmail.com>
2022-07-05 13:15:08 -0400

    componente cuando no hay nada seleccionado

---
+++ journal-app/src/components/journal/NothingSelected.jsx
@@ -0,0 +1,13 @@
+export const NothingSelected = () => {
+  return (
+    <div className='nothing__main-content'>
+      <p>
+        select something
+        <br />
+        or create an entry
+      </p>
+
+      <i className='far fa-star fa-4x mt-5'></i>
+    </div>
+  );
+};
