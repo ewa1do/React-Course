1cab47518dff6086409df33ad6435d61408613c0
ewa1do <this.eduardovera@gmail.com>
2022-07-17 13:54:21 -0400

    Configuraciones adicionales al calendario

--- calendar-app/src/components/calendar/CalendarScreen.jsx
+++ calendar-app/src/components/calendar/CalendarScreen.jsx
@@ -2,8 +2,12 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpes/calendar-messages';
 
+import 'moment/locale/es';
 import './calendar.scss';
+
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
@@ -13,12 +17,29 @@ const events = [
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
     bgcolor: '#fafafa',
+    notes: 'Comprar el pastel',
   },
 ];
 
+const eventStyleGetter = (event, start, end, isSelected) => {
+  console.log(event, start, end, isSelected);
+
+  const style = {
+    backgroundColor: '#367CF7',
+    borderRadius: '0px',
+    opacity: 0.8,
+    display: 'block',
+    color: '#fff',
+  };
+
+  return {
+    style,
+  };
+};
+
 export const CalendarScreen = () => {
   return (
-    <div className="calendar-screen">
+    <div className='calendar-screen'>
       <Navbar />
 
       <Calendar
@@ -26,6 +47,8 @@ export const CalendarScreen = () => {
         events={events}
         startAccessor='start'
         endAccessor='end'
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   );
