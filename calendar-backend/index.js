7a9a57f512ca6118bd3e6d0084bca23e32a95454
ewa1do <this.eduardovera@gmail.com>
2022-07-19 17:01:42 -0400

    Configurando express

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    ok: true,
+  });
+});
+
+const PORT = (process.env.PORT = 4000);
+const HOST_DEV = (process.env.HOST_DEV = '127.0.0.1');
+
+app.listen(PORT, HOST_DEV, () => {
+  console.log('Server Running on port: ' + PORT);
+});
