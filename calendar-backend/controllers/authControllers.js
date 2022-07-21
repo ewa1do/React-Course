e949290a38354ef4d592a944e08b1fe89f23e9a3
ewa1do <this.eduardovera@gmail.com>
2022-07-21 13:43:01 -0400

    Crear un usuario en nuestra base de datos

--- calendar-backend/controllers/authControllers.js
+++ calendar-backend/controllers/authControllers.js
@@ -1,15 +1,26 @@
 const { response } = require('express');
+const User = require('../models/UserModel');
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+const createUser = async (req, res = response) => {
+  // const { name, email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password,
-  });
+  try {
+    const user = new User(req.body);
+
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: 'register',
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador :/ ',
+    });
+  }
 };
 
 const loginUser = (req, res) => {
